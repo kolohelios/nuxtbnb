d526cfc5e3b688eff8aca92c97ab191caac162c4
Jon Edwards <jkedwards@me.com>
2022-01-01 18:36:28 -0800

    remove unnecessary red layout

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -26,7 +26,6 @@ export default {
     created() {
         const home = homes.find(home => home.objectID == this.$route.params.id)
         this.home = home
-    },
-    layout: 'red',
+    }
 }
 </script>
