9e2b1952aa888e38e6be94885fa93cb53ca74f7f
Jon Edwards <jkedwards@me.com>
2022-01-01 17:23:53 -0800

    add example data and begin rendering it

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,20 @@
-<template><div>Hello, world!</div></template>
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+        <img :src="home.images[0]" style="width:200px"><br/>
+        {{ home.title }}<br/>
+        {{ home.location.address }} {{ home.location.city }} {{ home.location.state }}<br/>
+        {{ home.guests }} guests<br/>
+        {{ home.pricePerNight }} / night<br/>
+    </div>
+  </div>
+</template>
+<script>
+import homes from "~/data/homes.json";
+
+export default {
+  data() {
+    return { homes: homes.slice(0, 3) };
+  },
+};
+</script>
