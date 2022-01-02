0f90a401d97dc3a287b7be89e893c5825c62d93a
Jon Edwards <jkedwards@me.com>
2022-01-01 19:21:57 -0800

    add map to property page using Google Maps

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -8,6 +8,8 @@
     <div><img src="/images/marker.svg" width="20" height="20" /> {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}</div>
     <div><img src="/images/star.svg" width="20" height="20" /> {{ home.reviewValue }}</div>
     <div>{{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bathrooms</div>
+    <div>{{ home.description }}</div>
+    <div style="height:800px;width:800px" ref="map"></div>
 </div>
 </template>
 <script>
@@ -15,7 +17,14 @@ import homes from '~/data/homes'
 export default {
     head() {
         return {
-            title: this.home.title
+            title: this.home.title,
+            script: [
+                {
+                    src: 'https://maps.googleapis.com/maps/api/js?key=<GoogleMapsAPIKey>&libraries=places',
+                    hid: 'map',
+                    defer: true,
+                }
+            ]
         }
     },
     data() {
@@ -26,6 +35,18 @@ export default {
     created() {
         const home = homes.find(home => home.objectID == this.$route.params.id)
         this.home = home
-    }
+    },
+    mounted() {
+        const mapOptions = {
+            zoom: 18,
+            center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng),
+            disableDefaultUI: true,
+            zoomControl: true,
+        }
+        const map = new window.google.maps.Map(this.$refs.map, mapOptions)
+        const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng)
+        const marker = new window.google.maps.Marker({ position })
+        marker.setMap(map)
+}
 }
 </script>
