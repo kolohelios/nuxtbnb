12ddc641c8173bed7ffe8de4872f289d7e48d8c2
Jon Edwards <jkedwards@me.com>
2022-01-01 17:30:39 -0800

    create module component for home

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
-    <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
-        <img :src="home.images[0]" style="width:200px"><br/>
-        {{ home.title }}<br/>
-        {{ home.location.address }} {{ home.location.city }} {{ home.location.state }}<br/>
-        {{ home.guests }} guests<br/>
-        {{ home.pricePerNight }} / night<br/>
+    <div>
+        <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+            <home-card :home="home"/>
+        </div>
     </div>
-  </div>
 </template>
 <script>
-import homes from "~/data/homes.json";
+import homes from '~/data/homes.json';
+import HomeCard from '~/components/HomeCard';
 
 export default {
-  data() {
-    return { homes: homes.slice(0, 3) };
-  },
+    components: {
+        HomeCard,
+    },
+    data() {
+        return { homes: homes.slice(0, 3) };
+    },
 };
 </script>
