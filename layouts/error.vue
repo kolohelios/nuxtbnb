e9ff974fd5da6a48b2a9c96971a4bbdfa8baa583
Jon Edwards <jkedwards@me.com>
2022-01-02 16:40:34 -0800

    add custom client error page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+<div style="text-align: center;">
+    <img src="https://www.hera.org.nz/wp-content/uploads/20150918_Notice_HERAreportR4-103Error_STRUC.jpg" width="200"/><br/><br/>
+    <h1>Error!</h1>
+    <div>{{ error.message }} {{ error.statusCode }}</div>
+</div>
+</template>
+<script>
+export default {
+    head() {
+        return {
+            title: 'BIG PROBLEMS!',
+        }
+    },
+    props: {
+        error: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
